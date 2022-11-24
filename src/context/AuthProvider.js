e3c922a22456c21875f6d75eddb2073fbad9c3e1
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-25 03:44:09 +0600

    setuser and google sign in done

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from 'firebase/auth';
 import app from '../Firebase/Firebase.config';
 
 
@@ -7,6 +7,7 @@ export const AuthContext = createContext();
 const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const createUser = (email, password) =>{
         setLoading(true);
@@ -24,12 +25,29 @@ const AuthProvider = ({children}) => {
         setLoading(true);
         return signOut(auth);
     }
+    const singInGoogle = (provider) =>{
+        setLoading(true)
+        return signInWithPopup(auth, provider)
+    }
+
+    useEffect( () =>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            console.log('user observing');
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, [])
 
    const authInfo={
     createUser,
     signIn,
     updateUser,
+    singInGoogle,
     logOut,
+    loading,
+    user
    }
     return (
         <AuthContext.Provider value={authInfo}>
