e5e717045b52dc78c65b81d9016639b73c13552e
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-25 18:46:37 +0600

    error page setup done

--- src/Router/Router/Router.js
+++ src/Router/Router/Router.js
@@ -3,12 +3,14 @@ import Main from "../../layouts/Main/Main";
 import Blogs from "../../Pages/Blogs/Blogs";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
+import DisplayError from "../../Pages/Shared/DisplayError/DisplayError";
 import SignUp from "../../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
     {
         path:'/',
         element: <Main></Main>,
+        errorElement: <DisplayError></DisplayError>
         children: [
             {
                 path: '/',
