d4fb7dea372517c85e1db22bc35a14724b79f98e
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-26 22:21:46 +0600

    dash board layout completed

--- src/Router/AdminRoute/AdminRoute.js
+++ src/Router/AdminRoute/AdminRoute.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { AuthContext } from '../../../context/AuthProvider';
+import { AuthContext } from '../../context/AuthProvider';
 import useAdmin from '../../hooks/useAdmin';
 
 import Loading from '../../Pages/Shared/Loading/Loading';
--- src/Router/Router/Router.js
+++ src/Router/Router/Router.js
@@ -7,6 +7,12 @@ import SignUp from "../../Pages/SignUp/SignUp";
 import DisplayError from '../../Pages/Shared/DisplayError/DisplayError'
 import Dashboard from "../../Pages/Dashboard/Dashboard";
 import Products from "../../Pages/Products/Products";
+import DashboardLayout from "../../layouts/DashboardLayout";
+import AdminRoute from "../AdminRoute/AdminRoute";
+import AllBuyers from "../../Pages/Dashboard/AllBuyers/AllBuyers";
+import AllSellers from "../../Pages/Dashboard/AllSellers/AllSellers";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import ReportedItems from "../../Pages/Dashboard/ReportedItems/ReportedItems";
 export const router = createBrowserRouter([
 
     {
@@ -38,10 +44,26 @@ export const router = createBrowserRouter([
                 element: <Products></Products>,
                 loader: ({params})=> fetch(`http://localhost:5000/category/${params.id}`)
             },
+            
+        ]
+    },
+    {
+        path: '/dashboard',
+        element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
+        errorElement: <DisplayError></DisplayError>,
+        children: [
             {
-                path: '/dashboard',
-                element: <Dashboard></Dashboard>,
-            }
+                path: '/dashboard/allbuyers',
+                element: <AdminRoute><AllBuyers></AllBuyers></AdminRoute>
+            },
+            {
+                path: '/dashboard/allsellers',
+                element: <AdminRoute><AllSellers></AllSellers></AdminRoute>
+            },
+            {
+                path: '/dashboard/reporteditems',
+                element: <AdminRoute><ReportedItems></ReportedItems></AdminRoute>
+            },
         ]
     }
 ])
